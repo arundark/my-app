ab2064f6736c6ff61a7a859893535f3dc40d9aeb
ArunArangan <arundark48@gmail.com>
2022-05-11 18:45:59 +0530

    connected with heroku

--- src/global.js
+++ src/global.js
@@ -1,1 +1,2 @@
-export const API = "https://62245f973af069a0f9b45751.mockapi.io";
+// export const API = "https://62245f973af069a0f9b45751.mockapi.io";
+export const API = "https://arundark-node.herokuapp.com";
